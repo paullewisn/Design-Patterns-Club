e356db1799a5eb9696d914ba085eac6ce4854613
Emily Sas <emily.sas@comparethemarket.com>
2017-02-21 11:17:12 +0000

    Adding exercise instructions

--- Decorator/Decorator/Decorator/Program.cs
+++ Decorator/Decorator/Decorator/Program.cs
@@ -9,13 +9,16 @@ namespace Decorator
     {
         static void Main(string[] args)
         {
-            Person superman = new SuperHero(); 
             Person pilot = new NonSuperButVeryTalentedIndividual();
+            pilot = new Flight(pilot);
 
+            Person superman = new SuperHero();
             superman = new Flight(superman);
             superman = new SuperStrength(superman);
+            // TODO give superman xray vision by creating a new decorator
+            // superman = new X-RayVision(superman);
 
-            pilot = new Flight(pilot);
+            // TODO create a new superhero with whichever powers you'd like
 
             Console.WriteLine("Superman: " + superman.GetAbilities());
             Console.WriteLine("Pilot: " + pilot.GetAbilities());
